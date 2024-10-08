570ec7f143aa53f8c2a2ba81c93c8241d4c74082
Gabriel Camara <galvares439@gmail.com>
2024-10-08 05:03:15 -0300

    commit

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,5 +1,6 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
+import Task from './components/Task'
 
 function App() {
 
@@ -9,6 +10,8 @@ function App() {
     prazo: '',
     horario: '',  
   })
+
+  const [tasks, setTasks] = useState([])
 
   const handleSubmit = async (e) => {
     e.preventDefault()
@@ -25,7 +28,10 @@ function App() {
       if (response.ok) {
         const data = await response.json();
         console.log('Tarefa criada:', data);
+
         setTask({ name: '', description: '', prazo: '', horario: '' });
+
+        setTasks([data, ...tasks]);
       } else {
         console.error('Erro ao criar a tarefa:', response.statusText);
       }
@@ -42,20 +48,43 @@ function App() {
     });
   };
 
+  useEffect(() => {
+    fetch('http://localhost:3000/tasks')
+      .then((response) => response.json())
+      .then((data) => setTasks(data))
+      .catch((error) => console.error('Erro ao buscar tarefas:', error))
+  }, [])
+
+  const handleDeleteTask = async (id) => {
+    try {
+      const response = await fetch(`http://localhost:3000/tasks/${id}`, {
+        method: 'DELETE',
+      })
+      if (response.ok) {
+        setTasks(tasks.filter(task => task.id !== id))     
+        console.log('Tarefa deletada com sucesso')
+      } else {
+        console.error('Erro ao deletar a tarefa:', response.statusText)
+      }
+
+    } catch (error) {
+      console.error('Erro ao deletar a tarefa:', error)
+    }
+  }
+
   return (
     <>
       <main>
-        <h1>To do list</h1>
-        <div>
+        <div className='creating-task'>
+          <h1>To do list</h1>
           <form onSubmit={handleSubmit}>
-            <button type='submit'>Create Task</button>
             <div className='label-input'>
               <label htmlFor='name'>Nome da tarefa</label>
               <input type="text" value={task.name} onChange={handleChange} name='name' required/>
             </div>
             <div className='label-input'>
               <label htmlFor='description'>Descrição da tarefa</label>
-              <textarea name="description" value={task.description} onChange={handleChange} resize="none" required></textarea>
+              <textarea name="description" value={task.description} onChange={handleChange} resize="none"></textarea>
             </div>
             <div className="label-input">
               <label htmlFor="prazo">Prazo</label>
@@ -65,8 +94,28 @@ function App() {
               <label htmlFor="horario">Horario</label>
               <input type="time" value={task.horario} onChange={handleChange} name="horario" required/>
             </div>
+            <button type='submit' className='create'>Create Task</button>
           </form>
         </div>
+          <div>
+            {tasks.length > 0 ? (
+              tasks.map((task) => (
+                <div className='task-app'>
+                  <Task
+                    key={task.id}
+                    id={task.id}
+                    name={task.name}
+                    description={task.description}
+                    prazo={task.prazo}
+                    horario={task.horario}
+                    onDelete={handleDeleteTask}
+                  />
+                </div>
+              ))
+            ) : (
+              <p>Nenhuma tarefa encontrada</p>
+            )}
+          </div>
         </main>
     </>
   )
--- ui/src/components/Task.jsx
+++ ui/src/components/Task.jsx
@@ -1,10 +1,27 @@
-import 'task.css'
+import "./task.css"
+import { XCircle } from "phosphor-react"
 
-function Task() {
+function Task({id, name, prazo, description, horario, onDelete}) {
+
+     const dataPrazo = new Date(prazo)
+     const dataFormatada = dataPrazo.toLocaleDateString('pt-BR')
+     const horarioFormatado = horario.slice(0, 5)
+
+     const handleDelete = () => {
+          onDelete(id)
+     }
+
      return (
           <>
                <div className="task">
-                    
+                    <div className="first-line">
+                         <div>
+                              <h3 className="name">{name}</h3>
+                              <p className="prazo">{dataFormatada}  às  {horarioFormatado}</p>
+                         </div>
+                         <XCircle size={40} className="apagar" onClick={handleDelete}/>
+                    </div>
+                    <textarea className="description" disabled>{description}</textarea>
                </div>
           </>
      )
